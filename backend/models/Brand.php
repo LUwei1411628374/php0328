4b8d4fe3c631be79b6bd3df23d8d60a423f81574
LUwei1411628374 <1411628374@qq.com>
2017-07-18 19:23:03 +0800

    phpstrom commit

---
+++ backend/models/Brand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "brand".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $intro
+ * @property string $logo
+ * @property integer $sort
+ * @property integer $status
+ */
+class Brand extends \yii\db\ActiveRecord
+{
+    public $imgFile;
+
+    public static function getStatusOptions($hidden_del=true){
+        $options=[
+            -1=>'删除',0=>'隐藏',1=>'正常'
+        ];
+        if($hidden_del){
+            unset($options['-1']);
+        }
+        return $options;
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'brand';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'intro','sort', 'status'], 'required'],
+
+            ['imgFile','file','extensions'=>['gif','png','jpg']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => '品牌名称',
+            'intro' => '简介',
+
+            'sort' => '排序',
+            'status' => '状态',
+            'imgFile'=>'LOGO'
+        ];
+    }
+}
