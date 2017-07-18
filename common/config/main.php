9df83e538beb9c1b035af67a9aaecfcade37b819
LUwei1411628374 <1411628374@qq.com>
2017-07-18 15:07:51 +0800

    初始化

---
+++ common/config/main.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+    //修改语言
+    'language'=>'zh-CN',
+];
